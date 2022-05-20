e9faa2288cd20cdd51cf7db42d9bd7c4372db190
Jared Wuliger <hello@jaredwuliger.com>
2022-05-20 12:20:15 -0400

    init

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+    appId: 'jmw.dictation.app',
+    appName: 'dictation',
+    webDir: 'www',
+    bundledWebRuntime: false,
+};
+
+export default config;
